b57f96dac0dfbb91f47abe0c192c94918f83df58
VictorHCassiano <victorhugocassianosilva@gmail.com>
2023-06-22 03:18:40 -0300

    exercicios

---
+++ Exercicios/src/Exercicio1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite um numero: ");
+		float resposta = sc.nextInt();
+		if (resposta>0) {
+			System.out.println("Positivo");
+		}
+		else {
+			System.out.println("Negativo");
+		}
+		sc.close();
+	}
+
+}
---
+++ Exercicios/src/Exercicio4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Exercicio4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Digite a primeira nota:");
+		float nota1 = sc.nextFloat();
+		System.out.print("Digite a segunda nota:");
+		float nota2 = sc.nextFloat();
+		
+		
+		System.out.println("Media:"+ (nota1+nota2)/2);
+		
+		
+		
+		sc.close();
+	}
+
+}
