cdcdcbac84724bf96eb709fe5086a578f7616e61
Victor Hugo <104807646+VictorHCassiano@users.noreply.github.com>
2023-06-22 16:22:21 -0300

    Trocar para if else

--- Exercicios/src/Exercicio3.java
+++ Exercicios/src/Exercicio3.java
@@ -5,24 +5,31 @@ public class Exercicio3 {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Digite um numero equivalente ao dia da semana");
 		int dia = sc.nextInt();
-		switch(dia) {
-			case 1:System.out.println("Domingo");
-			break;
-			case 2:System.out.println("Segunda");
-			break;
-			case 3:System.out.println("Terca");
-			break;
-			case 4:System.out.println("Quarta");
-			break;
-			case 5:System.out.println("Quinta");
-			break;
-			case 6:System.out.println("Sexta");
-			break;
-			case 7:System.out.println("Sabado");
-			break;
-			default:System.out.println("Valor inválido");
-			
+		if (dia ==1) {
+			System.out.println("Domingo");
 		}
+		else if(dia==2) {
+			System.out.println("Segunda");
+		}
+		else if(dia==3) {
+			System.out.println("Terça");
+		}
+		else if(dia==4) {
+			System.out.println("Quarta");
+		}
+		else if(dia==5) {
+			System.out.println("Quinta");
+		}
+		else if(dia==6) {
+			System.out.println("Sexta");
+		}
+		else if(dia==7) {
+			System.out.println("Sábado");
+		}
+		else {
+			System.out.println("Número invalido");
+		}
+		
 		sc.close();
 	}
 
