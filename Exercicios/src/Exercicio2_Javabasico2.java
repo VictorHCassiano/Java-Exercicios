9ecab99c7d4d9b7ab3394f6e9b266cd0082bd791
VictorHCassiano <victorhugocassianosilva@gmail.com>
2023-06-28 03:02:10 -0300

    Exercicios PDF 2

---
+++ Exercicios/src/Exercicio2_Javabasico2.java
@@ -0,0 +1,16 @@
+
+public class Exercicio2_Javabasico2 {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while (i<100) {
+			if(i%2 != 0 ) {
+				System.out.println(i);
+			}
+			i++;
+			
+		}
+
+	}
+
+}
