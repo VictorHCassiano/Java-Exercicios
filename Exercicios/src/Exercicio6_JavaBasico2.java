08f91153bfa9b5529689e4a581c63b36842e8c3b
VictorHCassiano <victorhugocassianosilva@gmail.com>
2023-06-30 02:23:54 -0300

    Exercicios PDF 2

---
+++ Exercicios/src/Exercicio6_JavaBasico2.java
@@ -0,0 +1,13 @@
+
+public class Exercicio6_JavaBasico2 {
+
+	public static void main(String[] args) {
+		for(int x = 1;x<=100;x++) {
+			if(x%3==0) {
+				System.out.print(x+" ");
+			}
+		}
+
+	}
+
+}
---
+++ Exercicios/src/Exercicio9_JavaBasico2.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Exercicio9_JavaBasico2 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
