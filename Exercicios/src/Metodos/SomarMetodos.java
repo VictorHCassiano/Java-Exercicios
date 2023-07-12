cb7e39bbb1a3ddb8217676dc91459a3b7c4cce7f
Victor Hugo <victorhugocassiano@hotmail.com>
2023-07-12 17:10:57 -0300

    Exercicios 12-07

--- Exercicios/src/Metodos/SomarMetodos.java
+++ Exercicios/src/Metodos/SomarMetodos.java
@@ -8,4 +8,10 @@ public class SomarMetodos {
 	int somar(int a,int b,int c) {
 		return a+b+c;
 	}
+	int media(int a,int b,int c) {
+		return (a+b+c)/3;
+	}
+	int media(int a,int b) {
+		return (a+b)/2;
+	}
 }
--- Exercicios/src/Metodos/UsarSoma.java
+++ Exercicios/src/Metodos/UsarSoma.java
@@ -6,6 +6,8 @@ public class UsarSoma {
 		SomarMetodos soma = new SomarMetodos();
 		int numero = soma.somar(10, 20);
 		System.out.println(numero);
+		int media = soma.media(10, 10);
+		System.out.println(media);
 	}
 
 }
